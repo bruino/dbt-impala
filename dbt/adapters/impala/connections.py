58d03303c693c0cb404c403835b6aeefc38371fd
Bruno Sarverry <bruino.one@gmail.com>
2023-09-01 20:02:10 -0300

    Update connections.py

--- dbt/adapters/impala/connections.py
+++ dbt/adapters/impala/connections.py
@@ -395,7 +395,11 @@ class ImpalaConnectionManager(SQLConnectionManager):
             fire_event(SQLQuery(conn_name=connection.name, sql=log_sql))
             pre = time.time()
 
-            cursor = connection.handle.cursor()
+            # cursor = connection.handle.cursor()
+            if connection.credentials.auth_type not in ["LDAP", "ldap", "GSSAPI", "gssapi", "kerberos"]:
+                cursor = connection.handle.cursor(user=connection.credentials.username)
+            else:
+                cursor = connection.handle.cursor()
 
             # paramstyle parameter is needed for the datetime object to be correctly quoted when
             # running substitution query from impyla. this fix also depends on a patch for impyla:
